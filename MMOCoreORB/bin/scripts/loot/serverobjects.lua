ce95b53eb616e85495021e5629b2f834c8ea4943
Detoxxe <DetoxxeGaming@gmail.com>
2018-05-14 07:03:24 -0400

    Added Base Server Code
    merged mtgserver files

---
+++ MMOCoreORB/bin/scripts/loot/serverobjects.lua
@@ -0,0 +1,8 @@
+-- Custom content
+includeFile("../custom_scripts/loot/serverobjects.lua")
+
+includeFile("items.lua")
+includeFile("groups.lua")
+
+includeFile("custom_loot/items.lua")
+includeFile("custom_loot/groups.lua")
