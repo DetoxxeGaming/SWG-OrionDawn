a1f2d53c63d6d7596a9e1695df3a38005c85cf83
Detoxxe <DetoxxeGaming@gmail.com>
2018-05-26 10:45:31 -0400

    removing old server files

--- MMOCoreORB/bin/scripts/loot/serverobjects.lua
+++
@@ -1,8 +0,0 @@
--- Custom content
-includeFile("../custom_scripts/loot/serverobjects.lua")
-
-includeFile("items.lua")
-includeFile("groups.lua")
-
-includeFile("custom_loot/items.lua")
-includeFile("custom_loot/groups.lua")
